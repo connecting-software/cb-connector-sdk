46beb2bdd21a41ada8064067c523b8eb3b464658
connecting-software <tfsservice@cn-s.eu>
2019-01-02 19:03:17 +0100

    Automated Release $(Release.ReleaseId)

--- examples/Attribute.cs
+++ examples/Attribute.cs
@@ -1,5 +1,5 @@
-public class Example
-{
+    public class Example
+    {
         //Define a property with ConnectorPropertyAttribute.
         [ConnectorProperty(
             Key = "Version",
@@ -9,15 +9,32 @@ public class Example
             DefaultValue = "v1.0",
             IsEncrypted = false)]
         public string Version { get; set; }
-		
-		//Define an OAuth property with ConnectorPropertyAttribute.
+
+        //Define an OAuth property with ConnectorPropertyAttribute.
+        //The flag will inform how to treat this property.
+        //The namespace should be used for grouping properties.
         [ConnectorProperty(
             Key = "ClientID",
             Name = "Client ID",
-            Description = "The client identifier issued to the client during the application registration process.",
+            Description = "The client identifier.",
             Flags = (ConnectorPropertyFlags) ((int) ConnectorPropertyFlags.AuthMask &
-                                              ((int) AuthenticationType.OAuth << 4)),
+                    ((int) AuthenticationType.OAuth2 << 4)),
             Namespace = "IOAuth2Configuration",
             IsEncrypted = false)]
         public string ClientId { get; set; }
-}
+
+        //Define an OAuth property with ConnectorPropertyAttribute.
+        //Shadows represents a dynamic object stored in CB Server.
+        [ConnectorProperty(
+            Key = "ExpiresAt",
+            Name = "ExpiresAt",
+            Description = "The token's expiration date.",
+            Flags = ConnectorPropertyFlags.Hidden | ConnectorPropertyFlags.ConnectorCanSet,
+            Namespace = "IOAuth2Configuration",
+            IsEncrypted = true)]
+        public DateTime ExpiresAt
+        {
+            get => Shadows.ExpiresAt;
+            set => Shadows.ExpiresAt = value;
+        }
+    }
--- examples/ReadData.cs
+++ examples/ReadData.cs
@@ -1,36 +1,46 @@
-public void ReadData(IResultSetLoader loader, IExecutionContext context, string connectionString, string queryString)
-{
-			using (var conn = new SqlConnection(connectionString))
-			{
-			    conn.Open();
-			    using (SqlCommand sqlcommand = new SqlCommand(queryString, conn))
-			    {
-			        //Retrieves a list of the variables (i.e. @ parameters).
-			        List<Variable> variables = context.GetAllVariables().ToList();
-			        variables.ForEach(v =>
-			        {
-			            sqlcommand.Parameters.Add(new SqlParameter(v.Name, context.GetVariableValue(v)));
-			        });
-			        var reader = sqlcommand.ExecuteReader();
-			        //Opens an ITableResultLoader object for writing. 
-			        using (ITableResultLoader tableLoader = loader.OpenTableResultLoader(ResultColumns))
-			        {
-			            if (reader.HasRows)
-			            {
-			                while (reader.Read() && 
-							            !tableLoader.CancellationToken.IsCancellationRequested)
-			                {
-			                    //Create a new ITableResultRow instance
-			                    ITableResultRow row = tableLoader.NewRow();
-			                    //Gets the values in the row, one per column.
-			                    reader.GetValues(row.GetValues());
-			                    //Adds a row to the ITableResultLoader.
-			                    tableLoader.Add(row);
-			                }
-			                reader.Close();
-			            }
-			        }
-			    }
+    public class Example
+    {
+        #region Example
+
+        public void ReadData(IResultSetLoader loader, IExecutionContext ctx, string connStr, string query)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand sqlcommand = new SqlCommand(query, conn))
+                {
+                    //Retrieves a list of the variables (i.e. @ parameters).
+                    List<Variable> variables = ctx.GetAllVariables().ToList();
+                    variables.ForEach(v =>
+                    {
+                        sqlcommand.Parameters.Add(new SqlParameter(v.Name, ctx.GetVariableValue(v)));
+                    });
+                    var reader = sqlcommand.ExecuteReader();
+                    //Opens an ITableResultLoader object for writing. 
+                    using (ITableResultLoader tableLoader = loader.OpenTableResultLoader(ResultColumns))
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read() &&
+                                        !tableLoader.CancellationToken.IsCancellationRequested)
+                            {
+                                //Create a new ITableResultRow instance
+                                ITableResultRow row = tableLoader.NewRow();
+                                //Gets the values in the row, one per column.
+                                reader.GetValues(row.GetValues());
+                                //Adds a row to the ITableResultLoader.
+                                tableLoader.Add(row);
+                            }
+                            reader.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 
-}
+
+
+
